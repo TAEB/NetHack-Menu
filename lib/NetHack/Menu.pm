526bc17727eb69bcddf5fdfbc62442660efeeb5f
Shawn M Moore <sartak@gmail.com>
2008-01-01 17:27:45 -0500

    Bump to 0.03

--- lib/NetHack/Menu.pm
+++ lib/NetHack/Menu.pm
@@ -175,11 +175,11 @@ NetHack::Menu - interact with NetHack's menus
 
 =head1 VERSION
 
-Version 0.02 released 01 Jan 07
+Version 0.03 released ???
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 

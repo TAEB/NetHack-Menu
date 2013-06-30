8230060590b4396a6c6414d87d14cd7b288e674a
Shawn M Moore <code@sartak.org>
2013-06-30 17:38:13 -0400

    Put state in the object

--- t/lib/MockVT.pm
+++ t/lib/MockVT.pm
@@ -4,27 +4,35 @@ use warnings;
 use parent 'Term::VT102';
 use NetHack::Menu;
 
-my @return_rows;
-my @checked_rows;
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+    $self->{return_rows} = [];
+    $self->{checked_rows} = [];
+    return $self;
+}
 
 sub checked_rows {
     my $self = shift;
-    return @checked_rows;
+    return @{ $self->{checked_rows} }
 }
 
 sub return_rows {
     my $self = shift;
-    push @return_rows, @_;
+    push @{ $self->{return_rows} }, @_;
 }
 
-sub next_return_row { shift @return_rows }
+sub next_return_row {
+    my $self = shift;
+    return shift @{ $self->{return_rows} };
+}
 
 sub rows { 24 }
 
 sub row_plaintext {
     my $self = shift;
-    push @checked_rows, shift;
-    return '' if $checked_rows[-1] == 0;
+    push @{ $self->{checked_rows} }, shift;
+    return '' if $self->{checked_rows}[-1] == 0;
     $self->next_return_row;
 }
 
@@ -33,7 +41,7 @@ sub checked_ok {
 
     local $Test::Builder::Level = $Test::Builder::Level + 1;
 
-    Test::More::is_deeply([splice @checked_rows], @_);
+    Test::More::is_deeply([splice @{ $self->{checked_rows} }], @_);
 }
 
 1;
